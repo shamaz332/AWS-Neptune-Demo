496e85275f1aefe66ec685717af0a6926d52320c
shamaz saeed <shamazsaeed332@gmail.com>
2021-02-24 15:30:44 +0500

    README Updated

---
+++ README.md
@@ -0,0 +1,5 @@
+## AWS Graph Database
+
+[Amazon Neptune](https://docs.aws.amazon.com/neptune/latest/userguide/graph-get-started.html)
+
+[Amazon Neptune Course](https://pages.awscloud.com/AWS-Learning-Path-Getting-Started-with-Amazon-Neptune_2020_LP_0009-DAT.html)
--- vpc_chat_bot/lib/vpc_chat_bot-stack.ts
+++ vpc_chat_bot/lib/vpc_chat_bot-stack.ts
@@ -1,9 +1,48 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as ec2 from "@aws-cdk/aws-ec2";
+import * as lambda from "@aws-cdk/aws-lambda";
+import * as apigw from "@aws-cdk/aws-apigateway";
 
 export class VpcChatBotStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    //================================
+    //Our VPC
+    //================================
+
+    const vpc = new ec2.Vpc(this, "VpcChatBot", {
+      
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: "Ingress",
+          subnetType: ec2.SubnetType.ISOLATED,
+        },
+      ],
+    });
+
+    //================================
+    //Our Lambda
+    //================================
+
+    const handler = new lambda.Function(this, "Lambda", { 
+      runtime: lambda.Runtime.NODEJS_10_X,
+      code: new lambda.AssetCode("functions"),
+      handler: "index.hello_world",
+      vpc: vpc,
+      vpcSubnets:
+        {
+          subnetType: ec2.SubnetType.ISOLATED                                                                                                               
+        }
+    });
+
+    //================================
+    //Our ApiGateWay
+    //================================
+
+    const apigateway = new apigw.LambdaRestApi(this, "apiForchat", {
+      handler: handler,
+    });
   }
 }
